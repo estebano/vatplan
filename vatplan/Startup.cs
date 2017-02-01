6862a014c1f79f34cf9bb25e203af3074ff3bfa6
hubert.tuszynski <hubert.tuszynski@S000014>
2017-02-01 15:52:14 +0100

    Add project files.

---
+++ vatplan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vatplan.Startup))]
+namespace vatplan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
